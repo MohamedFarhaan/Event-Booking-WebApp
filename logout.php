897c76aa828a35e0b77065370b30e8fb87225109
MohamedFarhaan <42342557+MohamedFarhaan@users.noreply.github.com>
2020-10-06 18:27:50 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if(isset($_POST['logoutButton'])){
+        session_destroy();
+        header("location:index.php");
+    }
+?>
+<html>
+    <head>
+        <title>Event Book</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar  navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a class="navbar-brand" href="#"><b>EVENT BOOK</b></a>
+                </div>
+            </div>
+        </nav>
+        <div style="padding-top:12.5%">
+            <center>
+            Are you sure, You want to <b>logout<b><br>
+            <br><form action="logout.php" method="POST">
+                <button type="submit"  name="logoutButton">Yes</button>
+                
+            </form><button onclick="window.location.href='index.php'">no</button>
+            </center>
+        </div>
+    </body>
+</html>
